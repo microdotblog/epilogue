36888f80a4d7a5312644f4ab8276054ad293d1c2
Manton Reece <manton@me.com>
2021-10-27 08:15:17 -0500

    Added initial Xcode project.

---
+++ Epilogue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Epilogue
+//
+//  Created by Manton Reece on 10/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
