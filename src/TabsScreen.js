72d47b994a5cbff8d153f847b39e6df8b256dd45
Manton Reece <manton@me.com>
2022-07-03 14:56:54 -0500

    Added tab bar icons.

--- src/TabsScreen.js
+++ src/TabsScreen.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { Text, View, useColorScheme } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { keys } from "./Constants";
 import { useEpilogueStyle } from './hooks/useEpilogueStyle';
+import { Icon } from './Icon';
 import epilogueStorage from "./Storage";
 import { HomeScreen } from "./HomeScreen";
 import { DiscoverScreen } from "./DiscoverScreen";
@@ -13,13 +14,20 @@ const Tab = createBottomTabNavigator();
 
 export function TabsScreen({ navigation }) {
 	const styles = useEpilogueStyle()
+    const is_dark = (useColorScheme() == "dark");
 
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name="Bookshelves" component={HomeScreen} options={{
-				headerTitle: ""
+			<Tab.Screen name="Bookshelves" component={HomeScreen} options={{				
+				headerTitle: "",
+				tabBarIcon: ({ focused, color, size }) => {
+					return <Icon name="bookshelves" color={is_dark ? "#FFFFFF" : "#337AB7"} size={18} />	
+				}
 			}} />
 			<Tab.Screen name="Discover" component={DiscoverScreen} options={{
+				tabBarIcon: ({ focused, color, size }) => {
+					return <Icon name="discover" color={is_dark ? "#FFFFFF" : "#337AB7"} size={18} />	
+				}
 			}} />
 		</Tab.Navigator>
 	);
