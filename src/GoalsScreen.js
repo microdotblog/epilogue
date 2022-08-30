7f5089354d4326127d5c785a36176516c5018b9a
Manton Reece <manton@me.com>
2022-08-29 22:19:41 -0500

    Updated to JSON Feed.

--- src/GoalsScreen.js
+++ src/GoalsScreen.js
@@ -30,14 +30,14 @@ export function GoalsScreen({ navigation }) {
 			
 			fetch("https://micro.blog/books/goals", options).then(response => response.json()).then(data => {
 				var new_goals = [];
-				for (let item of data) {
+				for (let item of data.items) {
 					var g = {
 						id: item.id,
-						name: item.name,
-						year: item.year,
-						value: item.value,
-						progress: item.progress,
-						isbns: [ "9781250852472", "9780857669469" ]
+						name: item.title,
+						year: item._microblog.goal_year,
+						value: item._microblog.goal_value,
+						progress: item._microblog.goal_progress,
+						isbns: item._microblog.isbns
 					};
 					
 					new_goals.push(g);
