46c1c3eeda9f89db8806e0dac8ec06e21ac49248
Vincent Ritter <git@vincentritter.com>
2025-12-29 15:09:28 +0100

    Add linking listener to delegate.

--- ios/epilogue/AppDelegate.swift
+++ ios/epilogue/AppDelegate.swift
@@ -38,6 +38,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     return true
   }
+
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    return RCTLinkingManager.application(app, open: url, options: options)
+  }
 }
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
